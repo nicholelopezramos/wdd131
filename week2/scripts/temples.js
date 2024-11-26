397ed3f17dbe5b54d81b495dfb88eda13bd49062
nicholelopezramos <nicholelopez587@gmail.com>
2024-11-26 21:47:04 +0800

    assignment..

---
+++ week2/scripts/temples.js
@@ -0,0 +1,6 @@
+    // Set current year in the footer's first paragraph
+    document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+    // Set last modified date in the footer's second paragraph
+    document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
+    
