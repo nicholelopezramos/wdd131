d36f78d6a0609dbf15d5edcb8888e29a0045e207
nicholelopezramos <nicholelopez587@gmail.com>
2024-11-26 22:32:41 +0800

    again

--- week2/scripts/temples.js
+++ week2/scripts/temples.js
@@ -1,6 +1,15 @@
-    // Set current year in the footer's first paragraph
-    document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.addEventListener("DOMContentLoaded", () => {
+    const yearSpan = document.getElementById("currentyear");
+    yearSpan.textContent = new Date().getFullYear();
 
-    // Set last modified date in the footer's second paragraph
-    document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
-    
+    const lastModified = document.getElementById("lastModified");
+    lastModified.textContent = `Last Modified: ${document.lastModified}`;
+});
+
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const menu = document.getElementById("menu");
+
+hamburgerBtn.addEventListener("click", () => {
+    menu.classList.toggle("hidden"); 
+    hamburgerBtn.textContent = menu.classList.contains("hidden") ? "☰" : "✖"; 
+});
