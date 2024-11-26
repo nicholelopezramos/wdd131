39b2fff31b8d7a76748753f742fc0450194edbbe
nicholelopezramos <nicholelopez587@gmail.com>
2024-11-26 22:56:44 +0800

    one

--- week2/scripts/temples.js
+++ week2/scripts/temples.js
@@ -5,11 +5,3 @@ document.addEventListener("DOMContentLoaded", () => {
     const lastModified = document.getElementById("lastModified");
     lastModified.textContent = `Last Modified: ${document.lastModified}`;
 });
-
-const hamburgerBtn = document.getElementById("hamburger-btn");
-const menu = document.getElementById("menu");
-
-hamburgerBtn.addEventListener("click", () => {
-    menu.classList.toggle("hidden"); 
-    hamburgerBtn.textContent = menu.classList.contains("hidden") ? "☰" : "✖"; 
-});
